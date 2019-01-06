00318d97f005466c49633855cd3830f59ba2a07e
Jamison Duarte da Silva <jamisonduarte23@gmail.com>
2019-01-06 15:22:19 -0200

    Commit Inicial

---
+++ Script.js
@@ -0,0 +1,3 @@
+window.onload = function alerta() {
+    alert("PS: Esse é um site teste com o objetivo de treinar e aprender desenvolvimento web. Qualquer conteudo aqui é puramente educativo.");
+}
